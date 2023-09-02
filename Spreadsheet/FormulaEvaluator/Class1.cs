171a91bed68901d97c1ab730c0fe1dea4c399e79
Frost <emandrews124@gmail.com>
2023-09-02 09:45:30 -0600

    Added testing file and method names.

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -1,7 +1,33 @@
-﻿namespace FormulaEvaluator
+﻿///
+/// This is an evaluation tool for evaluating infix expressions.
+/// 
+/// @author Ethan Andrews
+/// @version September 2, 2023
+///
+
+namespace FormulaEvaluator
 {
+    /// <summary>
+    /// This static class includes methods for evaluating infix expressions.
+    /// </summary>
     public static class Evaluator
     {
-        // TODO: Follow the PS1 instructions
+        /// <summary>
+        /// Delegate for returning variable values given a variable name "v".
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public delegate int Lookup(String v);
+
+        /// <summary>
+        /// Evaluates the infix expression "exp" using the Lookup function "variableEvaluator".
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <param name="variableEvaluator"></param>
+        /// <returns></returns>
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            return 0;
+        }
     }
 }
