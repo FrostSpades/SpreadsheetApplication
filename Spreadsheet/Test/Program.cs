fb6a6e7fc938c28f59df72bb649f88a1cb75c452
Frost <emandrews124@gmail.com>
2023-09-02 18:10:41 -0600

    Final version to be submitted.

--- Spreadsheet/Test/Program.cs
+++ Spreadsheet/Test/Program.cs
@@ -2,6 +2,15 @@
 
 int SampleEvaluator(String s)
 {
+    switch (s)
+    {
+        case "A1":
+            return 1;
+
+        case "B2":
+            return 2;
+    }
+
     return 0;
 }
 
@@ -12,14 +21,29 @@ void testRandomExpressions()
     Console.WriteLine(Evaluator.Evaluate("1+4*5-7", SampleEvaluator)); // 14
     Console.WriteLine(Evaluator.Evaluate("(2+3)/2 + 2", SampleEvaluator)); // 4
     Console.WriteLine(Evaluator.Evaluate("2+5/2-7", SampleEvaluator)); // -3
+    Console.WriteLine(Evaluator.Evaluate("4-54*3+7", SampleEvaluator)); // -151
 }
 
 void testExceptions()
 {
     //Console.WriteLine(Evaluator.Evaluate("2 + 7 * 4 / (2 + 5 - 7)", SampleEvaluator));
-
+    //Console.WriteLine(Evaluator.Evaluate("2+(3-7", SampleEvaluator));
+    //Console.WriteLine(Evaluator.Evaluate("2+(4-7))", SampleEvaluator));
+    //Console.WriteLine(Evaluator.Evaluate("54*23-(+7)", SampleEvaluator));
+    //Console.WriteLine(Evaluator.Evaluate("5*4+(+7)", SampleEvaluator));
+    //Console.WriteLine(Evaluator.Evaluate("-4*5+1", SampleEvaluator));
+    //Console.WriteLine(Evaluator.Evaluate("A4A", SampleEvaluator));
 }
 
+void testVariables()
+{
+    Console.WriteLine(Evaluator.Evaluate("A1+4", SampleEvaluator)); // 5
+    Console.WriteLine(Evaluator.Evaluate("A1+4*5-7", SampleEvaluator)); // 14
+    Console.WriteLine(Evaluator.Evaluate("(B2+3)/B2 + B2", SampleEvaluator)); // 4
+    Console.WriteLine(Evaluator.Evaluate("B2+5/B2-7", SampleEvaluator)); // -3
+    Console.WriteLine(Evaluator.Evaluate("4-54*3+7+A1", SampleEvaluator)); // -150
+}
 
-testRandomExpressions();
-testExceptions();
+//testRandomExpressions();
+//testExceptions();
+testVariables();
